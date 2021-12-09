1c163ae93b0caab827bc4d8a76e96e101a0b7179
tkwk0210 <guswls^123^^>
2021-12-09 20:50:47 +0900

    design? idontknow....6

--- app/src/main/java/com/example/cookingapp/MainActivity.java
+++ app/src/main/java/com/example/cookingapp/MainActivity.java
@@ -1,19 +1,42 @@
 package com.example.cookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     ImageView recipe, QR;
+    ViewPager2 viewPager ;
+
+
+    protected ArrayList setTextList(){
+
+        ArrayList<String> itemList = new ArrayList();
+        itemList.add("Page 1");
+        itemList.add("Page 2");
+        itemList.add("Page 3");
+        itemList.add("Page 4");
+        itemList.add("Page 5");
+
+        return itemList;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //뷰페이저
+        viewPager = findViewById(R.id.viewPager);
+        TextViewPagerAdapter pagerAdapter = new TextViewPagerAdapter(setTextList()) ;
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
 
         recipe = findViewById(R.id.recipe);
         QR = findViewById(R.id.QRcode);
@@ -21,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         intent.getStringExtra("load_URL");
-        // 웹뷰
+
 
         recipe.setOnClickListener(new View.OnClickListener() {
             @Override
